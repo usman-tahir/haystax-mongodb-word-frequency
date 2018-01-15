5017172f942a2bf98fb65c8e8eff0a32a88aded8
usman-tahir <tahir.usman.ali94@outlook.com>
2018-01-15 07:56:51 -0500

    feat: added pymongo functionality to push data to mLab

--- Utility.py
+++ Utility.py
@@ -1,6 +1,8 @@
 import json
 import datetime
 import random
+import sys
+from pymongo import MongoClient
 
 from classes.TrendingWords import *
 from classes.TrendingNode import *
@@ -18,7 +20,7 @@ def generate_trending_words_for_day(day_string, words):
     for x in range(0, 24):
         timestamp = datetime.datetime(day_string_parts[2], day_string_parts[0], day_string_parts[1], x, 0, 0).strftime("%m/%d/%Y %H:%M:%S")
         t = TrendingWords(timestamp, generate_trending_words(words))
-        trending_words_data.append(t)
+        trending_words_data.append(str(t))
     return trending_words_data
 
 def generate_day(trending_words_for_day):
@@ -28,15 +30,29 @@ def generate_day(trending_words_for_day):
         day.concatenate(TrendingNode(trending_words_for_day[x]))
     return day
 
+def generate_data_for_insertion(data):
+    with open("./data/frequencies.json", "w") as output:
+        json.dump(data, output)
+
+def update_mongo_collection():
+    word_frequency_data = [json.loads(data) for data in json.load(open("./data/frequencies.json"))]
+    print(word_frequency_data)
+    connection = MongoClient(sys.argv[1])
+    collection = connection.word_frequencies.word_frequencies
+    collection.insert(word_frequency_data)
+
 def main():
     words = read_words("./data/words.txt")
     data_for_collection = [] # contains a list of days represented as hour-based linked-lists
-    
-    # generate trends for sample days
+
     days = ["01/14/2018", "01/15/2018", "01/16/2018"]
     for day in days:
-        trend_for_day = generate_day(generate_trending_words_for_day(day, words))
-        data_for_collection.append(trend_for_day)
+        trending_words_data = generate_trending_words_for_day(day, generate_trending_words(words))
+        data_for_collection.append(trending_words_data)
+    data_for_collection = sum(data_for_collection, []) # flatten the nested lists
+
+    generate_data_for_insertion(data_for_collection)
+    update_mongo_collection()
     
 if __name__ == "__main__":
     main()
