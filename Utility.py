bea6702d2b9cf11200791c1246ace687a1abe66b
usman-tahir <tahir.usman.ali94@outlook.com>
2018-01-14 19:43:12 -0500

    feat: created a function to generate the hourly trending words for a certain day

--- Utility.py
+++ Utility.py
@@ -11,9 +11,19 @@ def read_words(filename):
 def generate_trending_words(words):
     return [random.choice(words) for x in range(0, 10)]
 
+def generate_trending_words_for_day(day_string, words):
+    day_string_parts = [int(part) for part in day_string.split("/")]
+    trending_words_data = []
+    
+    for x in range(0, 24):
+        timestamp = datetime.datetime(day_string_parts[2], day_string_parts[0], day_string_parts[1], x, 0, 0).strftime("%m/%d/%Y %H:%M:%S")
+        t = TrendingWords(timestamp, generate_trending_words(words))
+        trending_words_data.append(t)
+    return trending_words_data
+        
+
 def main():
-    words = generate_trending_words(read_words("./data/words.txt"))
-    print(words)
+    words = read_words("./data/words.txt")
 
 if __name__ == "__main__":
     main()
