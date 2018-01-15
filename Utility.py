83045fbbd417214529209e8c2981b638437d27fd
usman-tahir <tahir.usman.ali94@outlook.com>
2018-01-14 20:25:34 -0500

    feat: added functionality for creating hourly word trends based on a day

--- Utility.py
+++ Utility.py
@@ -20,10 +20,23 @@ def generate_trending_words_for_day(day_string, words):
         t = TrendingWords(timestamp, generate_trending_words(words))
         trending_words_data.append(t)
     return trending_words_data
-        
+
+def generate_day(trending_words_for_day):
+    day = TrendingNode(trending_words_for_day[0])
+
+    for x in range(1, 24):
+        day.concatenate(TrendingNode(trending_words_for_day[x]))
+    return day
 
 def main():
     words = read_words("./data/words.txt")
-
+    data_for_collection = [] # contains a list of days represented as hour-based linked-lists
+    
+    # generate trends for sample days
+    days = ["01/14/2018", "01/15/2018", "01/16/2018"]
+    for day in days:
+        trend_for_day = generate_day(generate_trending_words_for_day(day, words))
+        data_for_collection.append(trend_for_day)
+    
 if __name__ == "__main__":
     main()
